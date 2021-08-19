7824cc6399c9767ef90974e4733cce3b985dd98c
Trevor Benyack <trevorbenyack@gmail.com>
2021-08-19 16:24:09 -0400

    adds routing component to client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { NamespacesComponent } from './components/namespaces/namespaces.component';
 import { RoomsComponent } from './components/rooms/rooms.component';
 import { ChatWindowComponent } from './components/chat-window/chat-window.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NamespacesComponent,
     RoomsComponent,
-    ChatWindowComponent
+    ChatWindowComponent,
+    LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
